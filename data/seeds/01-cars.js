e1c47d48a6ade4cc706b5939d54909f31800c777
thekid510 <76237067+thekid510@users.noreply.github.com>
2022-03-22 23:41:22 -0700

    finished mvp

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,29 @@
-// STRETCH
+
+const cars = [
+    {
+        vin: '1111111111111',
+        make: 'toyota',
+        model:'prius',
+        mileage: 215000,
+        title: 'clean',
+        transmission: 'manual'
+}, {
+        vin: '2222222222222',
+        make: 'toyota',
+        model:'corolla',
+        mileage: 115000,
+        title: 'salvaged',
+       
+    }, {
+        vin: '3333333333333',
+        make: 'ford',
+        model:'focus',
+        mileage: 15000,
+        
+},
+]
+
+    exports.seed = async function (knex) {
+        await knex('cars').truncate()
+        await knex('cars').insert(cars)
+    }
